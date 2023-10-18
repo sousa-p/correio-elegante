83cf773da85916ddee5cd34b4bd494e92c2e2636
sousa-p <p.soousa7@gmail.com>
2023-10-17 21:02:08 -0300

    Add modulos em app.php

--- System/bootstrap/app.php
+++ System/bootstrap/app.php
@@ -93,10 +93,12 @@ $app->middleware([
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
-
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
+$app->register(App\Providers\EventServiceProvider::class);
+$app->register(\Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+$app->register(Laravel\Tinker\TinkerServiceProvider::class);
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
