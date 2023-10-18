b5dd952ef631125e05e10528382a05dd6358c53e
mygk-bea <meyaguskobeatriz@gmail.com>
2023-10-17 23:31:25 -0300

    Add frontEnd admin html/css

---
+++ FrontEnd/Admin/login/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="../../assets/css/main.css">
+  <link rel="stylesheet" href="./style_login.css">
+  <title>Login</title>
+</head>
+
+<body class="d-flex justify-content-center align-items-center">
+  <main class="d-flex flex-column justify-content-between align-items-center">
+    <div class="logo">
+      <img src="" alt="">
+    </div>
+
+    <div class="form__container">
+      <form action="" class="d-flex flex-column justify-content-between align-items-center">
+        <div class="d-flex flex-column">
+          <input type="text" id="name" placeholder="Nome">
+          <input type="password" id="password" placeholder="Senha">
+        </div>
+
+        <button class="form__btn">Entrar</button>
+      </form>
+    </div>
+  </main>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+
+</html>
