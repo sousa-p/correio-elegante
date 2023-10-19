3ede51521849c61730fe9181e6b7f1f7e6d9d493
mygk-bea <meyaguskobeatriz@gmail.com>
2023-10-18 23:01:10 -0300

    add html pag cliente -- add js listagemCartas

---
+++ FrontEnd/Admin/listagemCartas/script_listCartas.js
@@ -0,0 +1,16 @@
+const divCards = document.querySelectorAll(".card");
+const divCardsClosed = document.querySelectorAll(".card__closed");
+const divCardsContent = document.querySelectorAll(".card__content");
+
+divCardsClosed.forEach((element, i) => {
+  const card = divCards[i];
+  const classes = divCardsContent[i].classList;
+
+  element.addEventListener("click", () => {
+    classes.toggle("content__d-none");
+
+    card.style.outline = 
+    (!classes.contains("content__d-none")) 
+    ? "2px solid var(--tertiary-color)" : "0";
+  });
+});
