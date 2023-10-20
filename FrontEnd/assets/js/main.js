002e730429266c226cc923786040ff22e60db924
mygk-bea <meyaguskobeatriz@gmail.com>
2023-10-20 00:07:36 -0300

    fix css/js index.html -- feature add/remove candy

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -1,17 +1,27 @@
-// function changeDisplay()
+const divCandies = document.querySelector(".combos__candies");
+const addCandy = document.querySelector(".candy__btn-add");
+const candyFieldInner = `
+<select name="" id="" class="candy__option">
+  <option value="">R$00,00 Doces</option>
+  <option value="">R$00,00 Fini</option>
+  <option value="">R$00,00 Bala</option>
+</select>
+<button type="button" class="candy__btn-remove">
+  Remover
+  <img src="./assets/img/minus-outline.svg" alt="">
+</button>
+`;
 
-const addMessage = document.querySelector(".message__btn-add");
-const removeMessage = document.querySelector(".message__btn-remove");
-const fieldMessage = document.querySelector(".message>textarea");
+addCandy.addEventListener("click", () => {
+  var candyField = document.createElement('div');
+  candyField.classList.add("candy__field");
+  candyField.innerHTML = candyFieldInner;
+  divCandies.appendChild(candyField);
 
-addMessage.addEventListener("click", () => {
-    addMessage.style.display = "none";
-    removeMessage.style.display = "block";
-    fieldMessage.style.display = "block";
-});
-
-removeMessage.addEventListener("click", () => {
-    removeMessage.style.display = "none";
-    addMessage.style.display = "block";
-    fieldMessage.style.display = "none";
+  const removeCandy = document.querySelectorAll(".candy__btn-remove");
+  removeCandy.forEach(element => {
+    element.addEventListener("click", () => {
+      element.parentElement.remove();
+    });
+  });
 });
