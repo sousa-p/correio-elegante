2b4b42cf822557e76e9edc357fcdf9818beb1e56
mygk-bea <meyaguskobeatriz@gmail.com>
2023-10-21 18:53:55 -0300

    feature função de alterar tipo do forms js index

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -1,5 +1,33 @@
 const letterCouple = document.querySelector(".letter__couple");
 
+letterCouple.addEventListener("change", () => {
+  if(letterCouple.checked){
+    let secondRecipientData = document.createElement("div");
+    secondRecipientData.classList.add("personal__data-recipient");
+
+    let recipientData = document.querySelector(".personal__data-recipient");
+    secondRecipientData.innerHTML = recipientData.innerHTML;
+
+    let divRecipient = document.querySelectorAll(".personal__data")[1];
+    divRecipient.appendChild(secondRecipientData);
+
+    document.querySelector(".additional__btn-add").disabled = true;
+    document.querySelector(".disclaimer__combos").style.display = "block";
+
+    let additional = document.querySelectorAll(".additional__field");
+    additional.forEach((element) => {
+      element.remove();
+    });
+  } else {
+    let divRecipient = document.querySelectorAll(".personal__data")[1];
+    let secondRecipientData = document.querySelectorAll(".personal__data-recipient")[1];
+    divRecipient.removeChild(secondRecipientData);
+
+    document.querySelector(".additional__btn-add").disabled = false;
+    document.querySelector(".disclaimer__combos").style.display = "none";
+  }
+});
+
 const divAdditional = document.querySelector(".combos__additional");
 const addAdditional = document.querySelector(".additional__btn-add");
 
@@ -36,9 +64,7 @@ addAdditional.addEventListener("click", () => {
       AdditionalField.innerHTML += AdditionalInnerBtn;
       divAdditional.appendChild(AdditionalField);
 
-      const removeAdditional = document.querySelectorAll(
-        ".additional__btn-remove"
-      );
+      let removeAdditional = document.querySelectorAll(".additional__btn-remove");
       removeAdditional.forEach((element) => {
         element.addEventListener("click", () => {
           element.parentElement.remove();
