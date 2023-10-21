08beb912ff0839679ff4f1af2d18a45b9eba5e92
mygk-bea <meyaguskobeatriz@gmail.com>
2023-10-21 00:18:18 -0300

    feature listagem adicionais index

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -4,36 +4,46 @@ const divAdditional = document.querySelector(".combos__additional");
 const addAdditional = document.querySelector(".additional__btn-add");
 
 addAdditional.addEventListener("click", () => {
-  const url = 'http://127.0.0.1:8000/additional/';
+  const url = "http://127.0.0.1:8000/additional";
 
   fetch(url)
-  .then(response => response.json())
-  .then(dados => { 
-    console.log(dados)
-  })
-  .catch((_) => console.log(_))
+    .then((response) => response.json())
+    .then((dados) => {
+      let AdditionalField = document.createElement("div");
+      AdditionalField.classList.add("additional__field");
 
-  const AdditionalFieldInner = `
-  <select name="" id="" class="additional__option">
-    <option value="">R$00,00 Doces</option>
-    <option value="">R$00,00 Fini</option>
-    <option value="">R$00,00 Bala</option>
-  </select>
-  <button type="button" class="additional__btn-remove">
-    Remover
-    <img src="./assets/img/minus-outline.svg" alt="">
-  </button>
-  `;
+      let AdditionalInnerSelect = document.createElement("select");
+      AdditionalInnerSelect.classList.add("additional__select");
 
-  var AdditionalField = document.createElement('div');
-  AdditionalField.classList.add("additional__field");
-  AdditionalField.innerHTML = AdditionalFieldInner;
-  divAdditional.appendChild(AdditionalField);
+      let AdditionalInnerBtn = `
+      <button type="button" class="additional__btn-remove">
+        Remover
+        <img src="./assets/img/minus-outline.svg" alt="">
+      </button>
+      `;
 
-  const removeAdditional = document.querySelectorAll(".additional__btn-remove");
-  removeAdditional.forEach(element => {
-    element.addEventListener("click", () => {
-      element.parentElement.remove();
-    });
-  });
+      dados.shift();
+      dados.pop();
+      dados.forEach(element => {
+        let value = String(element.value.toFixed(2)).replace('.', ',');
+        let option = `
+        <option value="${element.id}">R$${value} ${element.name}</option>
+        `;
+        AdditionalInnerSelect.innerHTML += option;
+      });
+
+      AdditionalField.appendChild(AdditionalInnerSelect);
+      AdditionalField.innerHTML += AdditionalInnerBtn;
+      divAdditional.appendChild(AdditionalField);
+
+      const removeAdditional = document.querySelectorAll(
+        ".additional__btn-remove"
+      );
+      removeAdditional.forEach((element) => {
+        element.addEventListener("click", () => {
+          element.parentElement.remove();
+        });
+      });
+    })
+    .catch((_) => console.log(_));
 });
